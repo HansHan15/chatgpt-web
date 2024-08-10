3bc39d6b6f1b0704200cab6e5e37b83045b34e6d
hanzihan <hanzihan98@126.com>
2024-08-10 15:07:40 +0800

    test

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -10,7 +10,9 @@ export interface SettingsState {
 
 export function defaultSetting(): SettingsState {
   return {
-    systemMessage: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
+    // systemMessage: 'You are ChatGPT, a large language model trained by OpenAI. Follow the user\'s instructions carefully. Respond using markdown.',
+    systemMessage: '你是一个出题专家。请根据用户指定的主题、数量、题型、难度完成出题，每道题后需附有答案和解析。按照用户规定的格式回复。',
+
     temperature: 0.8,
     top_p: 1,
   }
